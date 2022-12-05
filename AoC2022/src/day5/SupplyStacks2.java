bcdcdcb9d9c680ef69ad70d92561b41b35f7a4e1
Jaiko657 <matthewboyd03@gmail.com>
2022-12-05 12:00:26 +0000

    Day 5: Supply Stacks

---
+++ AoC2022/src/day5/SupplyStacks2.java
@@ -0,0 +1,109 @@
+package day5;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class SupplyStacks2 {
+
+	private final static String INPUT_LOCATION = "C:\\Users\\Matthew Boyd\\Documents\\code\\advent-of-code-2022\\AoC2022\\src\\day5\\input.txt";
+
+	public static void main(String[] args) {
+		@SuppressWarnings("rawtypes")
+		ArrayList<Stack> stacks = new ArrayList<>();
+		// make array 0 indexed
+		stacks.add(null);
+		ArrayList<char[]> data = new ArrayList<>();
+
+		int height = 0; // algorithm for height
+		try {
+			BufferedReader heightReader = new BufferedReader(new FileReader(INPUT_LOCATION));
+			String heightLine = heightReader.readLine();
+			while (true) {
+				if (heightLine.charAt(1) == '1') {
+					heightReader.close();
+					break;
+				}
+				height++;
+				heightLine = heightReader.readLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		// puts crates into stacks in arrayList
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(new FileReader(INPUT_LOCATION));
+
+			for (int i = 0; i < height + 1; i++) {
+				String line = reader.readLine();
+				data.add(line.toCharArray());
+			}
+
+			int width = data.get(0).length;// algorithm for width
+			for (int j = 1; j < width - 1; j += 4) { // loop over each row
+				Stack<Character> stack = new Stack<Character>();
+				for (int i = height - 1; i >= 0; i--) {
+					char[] row = data.get(i);
+					char ch = row[j];
+					if (ch == ' ')
+						break;
+					stack.push(ch);
+				}
+				stacks.add(stack);
+			}
+
+			String line = reader.readLine();// skips blank line
+
+			// loops over instructions moving crates around stacks
+			line = reader.readLine();
+			while (line != null) {
+				String[] instruction = line.split(" move | from | to ");
+				instruction[0] = instruction[0].substring(5, instruction[0].length());
+				// parses integers
+				int amount = Integer.parseInt(instruction[0]);
+				int source = Integer.parseInt(instruction[1]);
+				int destination = Integer.parseInt(instruction[2]);
+				@SuppressWarnings("unchecked") // trust me bro
+				Stack<Character> sourceStack = stacks.get(source);
+				@SuppressWarnings("unchecked")
+				Stack<Character> destStack = stacks.get(destination);
+
+				//if lifting multiple crates move them to temporary stack
+				//first and then destination to prevent reversing order
+				if (amount != 0) {
+					@SuppressWarnings({ "rawtypes", "unchecked" })
+					Stack<Character> tmpStack = new Stack();
+					for (int i = 0; i < amount; i++) {
+						char currentValue = sourceStack.pop(); // remove from sourceStack
+						tmpStack.push(currentValue); // moves item to above stack
+					}
+					sourceStack = tmpStack;
+				}
+				for (int i = 0; i < amount; i++) {
+					char currentValue = sourceStack.pop();
+					destStack.push(currentValue);
+				}
+				line = reader.readLine();
+			}
+			// prints crates at top of stacks at end of instructions
+			boolean skip = true;
+			for (@SuppressWarnings("rawtypes")
+			Stack stack : stacks) {
+				if (skip) {
+					skip = false;
+					continue;
+				}
+				System.out.print(stack.pop());
+			}
+			reader.close();
+		} catch (
+
+		IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
